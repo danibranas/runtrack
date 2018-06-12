18ca1c5634173eaa9334d7bc06dba1d8f6fe0c0f
Daniel V. Brañas <daniel.branas@trabesoluciones.net>
2018-06-12 23:21:57 +0200

    Fix BackendApi implementation

--- app/src/main/java/com/muei/apm/runtrack/data/api/BackendApi.kt
+++ app/src/main/java/com/muei/apm/runtrack/data/api/BackendApi.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.muei.apm.runtrack.data.api.response.ApiResponse
 import com.muei.apm.runtrack.data.api.response.BackendApiResponse
 import com.muei.apm.runtrack.data.models.Event
+import org.json.JSONArray
 import org.json.JSONObject
 
 class BackendApi(context: Context): Api {
@@ -34,19 +36,28 @@ class BackendApi(context: Context): Api {
     }
 
     override fun findEventById(eventId: Long): ApiResponse<Event?> {
-        return getRequest("/events/$eventId", { _ -> Event(eventId) }) // FIXME
+        return getRequest("/events/$eventId", ::jsonToEvent)
     }
 
     override fun fetchNearEvents(lat: Long, lng: Long): ApiResponse<List<Event>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return getRequestList("/events", {
+            list ->
+                val result = ArrayList<Event>(list.length())
+
+                for (i in 0..(list.length() - 1)) {
+                    val item = list.getJSONObject(i)
+                    result.add(jsonToEvent(item))
+                }
+            result
+        })
     }
 
     override fun joinEvent(eventId: Long, userId: Long): ApiResponse<Event?> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return postRequest("/events/$eventId/$userId")
     }
 
     override fun finishEventById(eventId: Long, results: Event.Results?): ApiResponse<Any> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return postRequest("/events/$eventId")
     }
 
     private fun <T> getRequest(url: String, converter: (e: JSONObject) -> T): BackendApiResponse<T> {
@@ -57,5 +68,35 @@ class BackendApi(context: Context): Api {
                     Response.ErrorListener { error -> onError?.invoke(error.message!!) })
             addToRequestQueue(req)
         })
+    }
+
+    private fun <T> getRequestList(url: String, converter: (e: JSONArray) -> T): BackendApiResponse<T> {
+        return BackendApiResponse({
+            onSuccess, onError ->
+            val req = JsonArrayRequest(Request.Method.GET, "$BASE_URL$url", null,
+                    Response.Listener<JSONArray> { onSuccess(converter(it)) },
+                    Response.ErrorListener { error -> onError?.invoke(error.message!!) })
+            addToRequestQueue(req)
+        })
+    }
+
+    private fun <T> postRequest(url: String): BackendApiResponse<T> {
+        return BackendApiResponse({
+            _, onError ->
+            val req = JsonObjectRequest(Request.Method.GET, "$BASE_URL$url", null,
+                    null,
+                    Response.ErrorListener { error -> onError?.invoke(error.message!!) })
+            addToRequestQueue(req)
+        })
+    }
+
+    private fun jsonToEvent(obj: JSONObject): Event {
+        val ev = Event(obj.getLong("id"))
+        ev.name = obj.getString("name")
+        ev.users = obj.getInt("users")
+        ev.isInternal = obj.getBoolean("isInternal")
+        ev.imageUri = obj.getString("imageUri")
+        ev.description = obj.getString("description")
+        return ev
     }
 }
